958ea4786125134239e924a78a66c03a7dbef271
Julian Picht <julian.picht@gmail.com>
2020-04-22 14:32:27 +0200

    make guns upgradable

--- yic/gun.go
+++ yic/gun.go
@@ -13,6 +13,8 @@ type gun struct {
 	y float64
 
 	reloading float64
+
+	level int
 }
 
 func (g *gun) Tick(ms int) *engine.Transition {
@@ -27,13 +29,18 @@ func (g *gun) Tick(ms int) *engine.Transition {
 		return nil
 	}
 
+	var (
+		lvlDamage = float64(g.level+1) * gunDmg
+		lvlRange  = float64(g.level*10) + gunRange
+	)
+
 	for _, chain := range g.p.responsibilites.byChain {
 		for _, resp := range chain {
 			dist := math.Sqrt((g.x-resp.x)*(g.x-resp.x) + (g.y-resp.y)*(g.y-resp.y))
-			if dist <= gunRange {
+			if dist <= lvlRange {
 				// Shoot!
 				g.reloading = gunReload
-				resp.receiveDamage(gunDmg)
+				resp.receiveDamage(lvlDamage)
 				g.p.fxManager.addFXWithin("gun_shot", int(g.x), int(g.y), int(g.x)+14, int(g.y)+14)
 				g.p.fxManager.addFXWithin("gun_hit", int(resp.x), int(resp.y), int(resp.x)+12, int(resp.y)+12)
 				return nil
@@ -42,6 +49,24 @@ func (g *gun) Tick(ms int) *engine.Transition {
 	}
 
 	return nil
+}
+
+func (g *gun) Upgradable() bool {
+	return g.level < 3
+}
+
+func (g *gun) Upgrade() {
+	if g.level < 3 {
+		g.level += 1
+	}
+}
+
+func (g *gun) Cost() float64 {
+	return 2 * math.Pow10(g.level+3)
+}
+
+func (g *gun) Level() int {
+	return g.level
 }
 
 const gunRange = 40.0
